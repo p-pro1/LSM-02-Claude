b7725a2c49d1dc2b3d98747c06eb2e549a4c0f1e
Mac_Studio <piapro@web.de>
2024-07-06 15:27:54 +0200

    Initial commit from Create Next App

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBTVTUxhQhxQza3qaM3x2Nc0acpiU03aMI",
+    authDomain: "lsm-event-site-2-claude.firebaseapp.com",
+    projectId: "lsm-event-site-2-claude",
+    storageBucket: "lsm-event-site-2-claude.appspot.com",
+    messagingSenderId: "596056332316",
+    appId: "1:596056332316:web:464d5f5464f2ba57c767d9"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
